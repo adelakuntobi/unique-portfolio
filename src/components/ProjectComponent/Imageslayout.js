efc9c2e608c397954d142f9537e49dec7dd45c55
adelakuntobi <adelakuntobiloba1@gmail.com>
2020-09-12 11:47:11 +0100

    Added favicon

--- src/components/ProjectComponent/Imageslayout.js
+++ src/components/ProjectComponent/Imageslayout.js
@@ -5,7 +5,7 @@ import React from 'react'
 function Imageslayout(props) {
   const { link,loc1,loc2,loc3,loc4,loc5,loc6,loc7,loc8, dalt1,dalt2,dalt3,dalt4,dalt5,dalt6,dalt7,dalt8} = props
   return (
-    <div className="w-full lg:w-9/12 mx-auto p-6">
+    <div className="w-full lg:w-9/12 container mx-auto p-6">
       <img className="w-full my-6" src={loc1} alt={dalt1} />
       <img className="w-full my-6" src={loc2} alt={dalt2} />
       <img className="w-full my-6" src={loc3} alt={dalt3} />
